313eef9f72bcb3b43ad8030c66bc9eab8c3b33b9
PLM-RafaelTorres <PLMrstorres@proton.me>
2023-09-21 09:33:05 +0800

    added test 3 for activity 2

---
+++ activity 2/Activity2Test3.java
@@ -0,0 +1,19 @@
+public class Activity2Test3{
+    public static void main(String[] args) {
+        // The code below has an error, identify the error and make a general conclusion
+        boolean blFound = false;
+        switch(blFound)
+        {
+            case true:  System.out.println("Hello");
+                        break;
+            case false: System.out.println("Goodbye");
+                        break;
+            default:    System.out.println("This is never executed");
+        }
+
+        // In Java, this code will not compile as switch statements cannot be performed on a value of type boolean. 
+        // It only works on byte, short, char, int and a few special classes that wrap certain primitive types: Character, Byte, Short, and Integer.
+        // Even if the code were to work, the default statement will never be executed as the switch statement will always evaluate to either true or false only.
+        // Therefore, to evaluate boolean expressions, it is much more intuitive to use if-else statements instead.  
+    }
+}
